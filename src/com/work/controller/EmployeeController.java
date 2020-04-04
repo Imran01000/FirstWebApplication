c83d9ea41253c0ca350ea9b7fc41745bd42591d4
Imran01000 <imshaikh01000@gmail.com>
2020-04-04 19:49:32 +0530

    ADDED:Added file which defines employee servlet

---
+++ src/com/work/controller/EmployeeController.java
@@ -0,0 +1,49 @@
+package com.work.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.work.dao.EmployeeDAO;
+import com.work.dao.EmployeeDAOimplement;
+import com.work.entity.Employee;
+
+
+public class EmployeeController extends HttpServlet 
+{
+	private static final long serialVersionUID = 1L;
+     
+	//CREATE A REF VARIABLE FOR EmployeeDAO.
+	EmployeeDAO employeeDAO = null;
+
+	//CREATE CONSTRUCTOR AND INITIALIZE EmployeeDAO.
+	public EmployeeController()
+	{
+		employeeDAO = new EmployeeDAOimplement();
+	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		//CALL DAO METHOD TO GET THE LIST OF EMPLOYEE.
+		List<Employee> list = employeeDAO.get();
+		
+		//ADD THE EMPLOYESS TO REQUEST OBJECT.
+		request.setAttribute("list", list);
+		
+		//GET THE REQUEST DISPATCHER.
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/views/employee_list.jsp");
+		
+		//FORWARD THE REQUEST AND RESPONSE OBJECTS.
+		dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		
+	}
+
+}
