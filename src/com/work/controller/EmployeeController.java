7c454559021cc5d798051e64ff1060b56b75eecd
Imran01000 <imshaikh01000@gmail.com>
2020-04-05 16:45:07 +0530

    MODIFIED:Fetching the datda from add table to servlet and save it to database

--- src/com/work/controller/EmployeeController.java
+++ src/com/work/controller/EmployeeController.java
@@ -17,7 +17,9 @@ import com.work.entity.Employee;
 public class EmployeeController extends HttpServlet 
 {
 	private static final long serialVersionUID = 1L;
-     
+	
+	RequestDispatcher dispatcher = null;
+
 	//CREATE A REF VARIABLE FOR EmployeeDAO.
 	EmployeeDAO employeeDAO = null;
 
@@ -26,29 +28,36 @@ public class EmployeeController extends HttpServlet
 	{
 		employeeDAO = new EmployeeDAOimplement();
 	}
-	
-	
+
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
-		//CALL DAO METHOD TO GET THE LIST OF EMPLOYEE.
-		List<Employee> list = employeeDAO.get();
 		
-		//ADD THE EMPLOYESS TO REQUEST OBJECT.
-		request.setAttribute("list", list);
-		
-		//GET THE REQUEST DISPATCHER.
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/views/employee_list.jsp");
-		
-		//FORWARD THE REQUEST AND RESPONSE OBJECTS.
-		dispatcher.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
+		int  id = Integer.parseInt(request.getParameter("id"));
 		String name = request.getParameter("fname");
 		String dob = request.getParameter("dob");
 		String role = request.getParameter("designation");
-		System.out.println(name+" "+dob+" "+role);
+		Employee employee = new Employee();
+		employee.setId(id);
+		employee.setName(name);
+		employee.setDob(dob);
+		employee.setDepartment(role);
+		if(employeeDAO.save(employee))
+		{
+			request.setAttribute("message", "Save successfully!!");
+		}
+
+		//GET THE REQUEST DISPATCHER.
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/views/employee_list.jsp");
+
+		//FORWARD THE REQUEST AND RESPONSE OBJECTS.
+		dispatcher.forward(request, response);
 	}
+	
+	
 
 }
